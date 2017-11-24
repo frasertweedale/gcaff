b220ba8653a3a9057a2bf45b3ed9dde4cf35c9c6
Fraser Tweedale <frase@frase.id.au>
2017-11-24 22:04:29 +1000

    gpg.py: (C) 2017

--- gcaff/gpg.py
+++ gcaff/gpg.py
@@ -1,5 +1,5 @@
 # This file is part of gcaff
-# Copyright (C) 2013, 2014, 2016  Fraser Tweedale
+# Copyright (C) 2013, 2014, 2016, 2017  Fraser Tweedale
 #
 # gcaff is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
