35b53041e9d12c9d0517951064bf7c48e3ff9f7f
Fraser Tweedale <frase@frase.id.au>
2017-11-24 22:04:40 +1000

    bump version to v0.4

--- gcaff/version.py
+++ gcaff/version.py
@@ -14,6 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = '0.3'
+VERSION = '0.4'
 USER_AGENT = 'gcaff {} - https://github.com/frasertweedale/gcaff' \
     .format(VERSION)
